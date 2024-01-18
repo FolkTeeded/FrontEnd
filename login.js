0bb7bd9e770a0047fb85314e58451006bbb86f85
Tonphai44 <phaizaza21@gmail.com>
2024-01-18 20:12:13 +0700

    get new

---
+++ login.js
@@ -0,0 +1,20 @@
+const btn_login = document.getElementById('loginpress')
+btn_login.style.backgroundColor = 'Green'
+const userid_1 = document.getElementById('userid')
+const password_1 = document.getElementById('password')
+
+function btn_login_2 (){
+    console.log(userid_1.value)
+const uid = userid_1.value
+const ps = password_1.value
+    console.log(ps)
+}
+
+btn_login.onclick = btn_login_2 
+
+
+
+
+
+
+
