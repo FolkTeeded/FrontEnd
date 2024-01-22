7a7d296c43329871f145ae592da5d237494d4688
FolkTeeded <138186435+FolkTeeded@users.noreply.github.com>
2024-01-23 03:01:25 +0700

    updata

--- login.js
+++ login.js
@@ -24,3 +24,46 @@ $("#loginpress").on("click", function() {
 
     }
 });
+
+$(".login_register").on("click", function() {
+    console.log("Hello");
+    
+    var direction = "next1"; 
+    
+    if (direction === "next1") {
+        $("div[class^=overlay_register]").addClass('open');
+
+    }
+});
+$(".prev").on("click", function() {
+    console.log("Hello");
+
+    var direction = "next1"; 
+
+    if (direction === "next1") {
+        $("div[class^=overlay]").removeClass('open');
+    }
+});
+
+
+$(".login_pass").on("click", function() {
+    console.log("Hello");
+
+    var direction = "next1"; 
+
+    if (direction === "next1") {
+        $("div[class^=overlay_pass]").addClass('open');
+    }
+});
+$(".prev1").on("click", function() {
+    console.log("Hello");
+
+    var direction = "next1"; 
+
+    if (direction === "next1") {
+        $("div[class^=overlay_pass]").removeClass('open');
+    }
+});
+
+
+
