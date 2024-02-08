5e4a6cd82628b112c6aea6ecf624cb1ffcedce90
Tonphai44 <phaizaza21@gmail.com>
2024-02-08 23:02:48 +0700

    //

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
-const apilogin = 'http://localhost:3000/api/login'
+const apilogin = 'https://5101-113-53-150-169.ngrok-free.app/api/login'
+const apiregister = 'https://5101-113-53-150-169.ngrok-free.app/api/register'
+const
+
 
 const btn_login = document.getElementById('loginpress')
 // btn_login.style.backgroundColor = ''
@@ -162,7 +165,7 @@ async function insert_data(a, b, c, d, p, passComfim) {
 
         $.ajax({
             type: 'post',
-            url: `http://localhost:3000/api/register`,
+            url: apiregister,
             contentType: "application/json",
             headers: { "Authorization": localStorage.getItem('token') },
             data: JSON.stringify({
